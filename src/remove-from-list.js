18884db9e833eb7ddeb8f0481fb6a2d32fd7ff5b
DeguzBelarus <deguz@mail.ru>
2022-10-16 22:23:10 +0300

    solve remove-from-list task (without 1 test)

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l == null) {
+    return l;
+  }
+
+  while (l.value === k && l.next.value && l.next.value !== k) {
+    l = l.next;
+  }
+
+  let node = l,
+    last;
+
+  if (node && node.value === k) {
+    return node.next;
+  }
+
+  while (node && node.value !== k) {
+    last = node;
+    node = node.next;
+  }
+
+  if (last && node.value === k) {
+    // if (node.next.value === k) {
+
+    last.next = node.next;
+    // }
+  }
+
+  return l;
+
+  //  while (l.value == k) {
+  //    l = l.next;
+  //  }
+
+  //  currentNode = l;
+  //  nextNode = currentNode.next;
+
+  //  while (nextNode != null) {
+  //    if (nextNode.value == k) {
+  //      currentNode.next = nextNode.next;
+
+  //      if (currentNode.next == null) {
+  //        break;
+  //      }
+
+  //      currentNode = currentNode.next;
+  //      nextNode = currentNode.next;
+  //    }
+  //  }
+
+  //  return l;
+
+  //   if (l.value === k) {
+  //     return l.next ? removeKFromList(l.next, k) : null;
+  //   } else {
+  //     return {
+  //       next: removeKFromList(l.next, k),
+  //       value,
+  //     };
+
+  //    //  return
+  //   }
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
